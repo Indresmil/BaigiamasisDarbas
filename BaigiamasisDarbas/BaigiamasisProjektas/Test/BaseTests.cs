c5d8aba634e13a51b7b8cae64df0843195662474
Indresmil <72129098+Indresmil@users.noreply.github.com>
2021-01-11 20:21:17 +0200

    Trecias

---
+++ BaigiamasisDarbas/BaigiamasisProjektas/Test/BaseTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaigiamasisDarbas.BaigiamasisProjektas.Test
+{
+    class BaseTests
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://letenos.lt/";
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            
+        }
+        [TearDown]
+        public void AfterTest()
+        {
+            driver.Quit();
+        }
+
+    }
+}
