1ec5248e0b7479d920107c89cc2abb6a5e3ec508
Indresmil <72129098+Indresmil@users.noreply.github.com>
2021-01-12 14:03:31 +0200

    Penktas

--- BaigiamasisDarbas/BaigiamasisProjektas/Pages/HomePage.cs
+++ BaigiamasisDarbas/BaigiamasisProjektas/Pages/HomePage.cs
@@ -1,4 +1,6 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,11 +13,48 @@ namespace BaigiamasisDarbas.BaigiamasisProjektas.Pages
 
         private IWebElement searchIcon => driver.FindElement(By.ClassName("icon-magnifier"));
         private IWebElement search => driver.FindElement(By.Name("s"));
+        private IWebElement item => driver.FindElement(By.LinkText("DOG Copenhagen petnešos Comfort walk Pro NAUJAS MODELIS"));
+        private IWebElement color => driver.FindElement(By.CssSelector("#pa_dydis"));
+        private IWebElement colorSelect => driver.FindElement(By.CssSelector("#pa_spalva > option:nth-child(7)"));
+        private IWebElement size => driver.FindElement(By.CssSelector("#pa_dydis"));
+        private IWebElement sizeSelect => driver.FindElement(By.CssSelector("#pa_dydis > option:nth-child(2)"));
 
         public void NavigateToSearch()
         {
             searchIcon.Click();
         }
+        public void CorrectSearch()
+        {
+            search.SendKeys("petnešos" + Keys.Enter);
+        }
+
+        public void CorrectSearhClick()
+        {
+            item.Click();
+        }
+        public void AssertCorrectSearch()
+        {
+            Assert.AreEqual("DOG Copenhagen petnešos Comfort walk Pro NAUJAS MODELIS", driver.FindElement(By.LinkText("DOG Copenhagen petnešos Comfort walk Pro NAUJAS MODELIS")).Text);
+        }
+
+        public void SelectColor()
+        {
+            colorSelect.Click();
+        }
+
+        public void AssertColorSelect()
+        {
+            Assert.AreEqual("Mėlyna", driver.FindElement(By.CssSelector("#pa_spalva > option:nth-child(7)")).Text);
+        }
+        public void SelectSize()
+        {
+            sizeSelect.Click();
+        }
+        public void AssertSizeSelect()
+        {
+            Assert.AreEqual("L", driver.FindElement(By.CssSelector("#pa_dydis > option:nth-child(2)")).Text);
+        }
+
         public void SearchFail()
         {
             search.SendKeys("$" + Keys.Enter);
---
+++ BaigiamasisDarbas/BaigiamasisProjektas/Test/Home.cs
@@ -0,0 +1,43 @@
+﻿using BaigiamasisDarbas.BaigiamasisProjektas.Pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaigiamasisDarbas.BaigiamasisProjektas.Test
+{
+    class Home : BaseTests
+    {
+        private HomePage homePage;
+
+
+        [SetUp]
+
+        public void Beforetest()
+        {
+            homePage = new HomePage(driver);
+        }
+
+        [Test]
+
+        public void CorrectSearch()
+        {
+            homePage.NavigateToSearch();
+            homePage.CorrectSearch();
+            homePage.AssertCorrectSearch();   
+        }
+
+        [Test]
+        public void SelectColorAndSize()
+        {
+            homePage.NavigateToSearch();
+            homePage.CorrectSearch();
+            homePage.CorrectSearhClick();
+            homePage.SelectColor();
+            homePage.SelectSize();
+            homePage.AssertColorSelect();
+            homePage.AssertSizeSelect();
+        }
+
+    }
+}
